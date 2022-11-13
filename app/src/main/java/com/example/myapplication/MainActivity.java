06b7a601155eb742f22890a14f20ac7561d42ced
Arman Febriyanto <armanfebriyanto27@gmail.com>
2022-11-13 15:12:16 +0700

    memindahkan activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain);
-        setContentView(R.layout.activitymain2);
+
+        btn3 =findViewById(R.id.button4);
+
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent( MainActivity.this, MainActivity3.class);
+                startActivity(pindah);
+            }
+        });
     }
 }
